c0ae79dde79338587ff03fa7746f521a27291aa0
Kaya Winn <mail@psyreal.net>
2017-04-28 13:09:49 -0700

    adds update function

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-  delete(question) {
-    if (confirm('Are you sure you want to delete this question?')) {
+    update(question, params) {
+      this.sendAction('update', question, params);
+    },
+
+    delete(question) {
+      if (confirm('Are you sure you want to delete this question?')) {
       this.sendAction('destroyQuestion', question);
+      }
     }
   }
-}
 });
--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+    actions: {
+      updateQuestionForm() {
+        this.set('updateQuestionForm', true);
+      },
+      update(question) {
+        var params = {
+          title: this.get('title'),
+          body: this.get('body'),
+          author: this.get('author'),
+          date: this.get('date'),
+          };
+        this.set('updateQuestionForm', false);
+        this.sendAction('update', question, params);
+      }
+    }
+
 });
